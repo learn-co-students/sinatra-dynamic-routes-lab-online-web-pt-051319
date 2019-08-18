5d473765f761b89aa69b3da21243b21fd66295ca
lexus2727 <lexus2424@yahoo.com>
2019-08-18 19:32:51 -0400

    Done.

--- app.rb
+++ app.rb
@@ -7,43 +7,39 @@ class App < Sinatra::Base
     "#{@name}"
  end
 
- get '/square/:number' do
+  get '/square/:number' do
   @num = params[:number].to_i
   "#{@num **2}"
-end
+  end
 
-get '/say/:number/:phrase' do
+  get '/say/:number/:phrase' do
   @num = params[:number].to_i
   @phrase = params[:phrase]
   "#{@phrase}\n" * @num
-end
+  end
 
-get '/say/:word1/:word2/:word3/:word4/:word5' do
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
   "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
-end
+  end
 
-get '/:operation/:number1/:number2' do
+  get '/:operation/:number1/:number2' do
   @operation = params[:operation]
   @num1 = params[:number1].to_i
   @num2 = params[:number2].to_i
 
   case @operation
 
-  when "subtract"
-    (@num2 - @num1).to_s
+    when "subtract"
+     (@num2 - @num1).to_s
   
-  when "add"
-    (@num1 + @num2).to_s
+    when "add"
+     (@num1 + @num2).to_s
 
-  when "multiply"
-    (@num1 * @num2).to_s
+    when "multiply"
+      (@num1 * @num2).to_s
 
-  when "divide"
-    (@num1 / @num2).to_s
+    when "divide"
+      (@num1 / @num2).to_s
+    end
   end
-end
-
-
-
-  
 end
